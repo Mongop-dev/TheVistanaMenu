5e4f0847c1eab09f239577e88e2f776a353e0a10
Igwe Ezekiel <igweezekiel2021@gmail.com>
2024-12-05 17:52:46 +0100

    new commit

--- scripts/c-menu.js
+++ scripts/c-menu.js
@@ -30,8 +30,27 @@ const menuData = {
             name: "Bircher Muesli",
             price: 5500,
             description: "The Classical swiss vitamin shot with oak flakes, yoghurt, and seasonal fruit"
-        }
-    
+        },
+        {
+            name: "Fresh Fruit Salad",
+            price: 4800,
+            description: "Fresh Fruit Salad for You"
+        },
+        {
+            name: "Eggs And Noodles",
+            price: 7000,
+            description: "Duo of Fried or Boiled Eggs With Noodles"
+        },
+        {
+            name: "Two Eggs Omelette",
+            price: 4500,
+            description: "",
+        },
+        {
+            name: "Spanish Omelette",
+            price: 5800,
+            description: "Two Eggs Omelette with Onions, Tomatoes, And Green Pepper"
+        },
     ],
     "Sandwiches": [
         {
@@ -42,29 +61,208 @@ const menuData = {
         {
             name: "Prawns Club Sandwich",
             price: 15000,
-            description: "Curry-Boiled Prawns. Served with French Fries"
+            description: "A Change from the Original Recipe Made with Curry-Boiled Prawns. Served with French Fries"
         },
         {
             name: "Hamburger",
             price: 10000,
             description: "150 gr. Ground Meat Patty with Secret Homemade Sauce. Served with French Fries."
+        },
+        {
+            name: "Cheeseburger",
+            price: 11000,
+            description: "We add to the Original Hamburger recipe. A Slice of Cheddar Cheese served with French Fries"
+        },
+        {
+            name: "Croque Monsieur",
+            price: 10000,
+            description: " From France, This Golden Brown Toasted Ham and Cheese Sandwich is served with French Fries and Coleslaw"
+        },
+        {
+            name: "Chicken Burger",
+            price: 15000,
+            description: "We Add to the Original Hamburger Recipe. A Slice of Cheddar Cheese served with French Fries"
+        },
+        {
+            name: "Fresh Spring Rolls",
+            price: 6000,
+            description: "(4 Pieces) with Dipping Sauce"
+        },
+        {
+            name: "Fresh Samosa",
+            price: 6000,
+            description: "(4 Pieces) with Dipping Sauce"
+        },
+        {
+            name: "Chicken Wrap",
+            price: 10000,
+            description: "comes with French fries"
+        },
+        {
+            name: "Italian Bruschetta",
+            price: 5000,
+            description: "2 Slices of Toasted Bread Gently Brushed with Garlic and Topped with Olive Oil, Fresh Tomato, and Oregano"
         }
     ],
     "Small Chops": [
         {
             name: "Meat Pie",
             price: 2000,
-            description: "Standard Meat Pie"
+            description: "1 Standard Meat Pie"
         },
         {
             name: "Chicken Pie",
             price: 2000,
-            description: "Standard Chicken Pie"
+            description: "1 Standard Chicken Pie"
         },
         {
             name: "Scotch Egg",
             price: 3000,
             description: "2 Scotch Eggs"
+        },
+        {
+            name: "Fish Pie",
+            price: 2000,
+            description: "1 Standard Fish Pie"
+        },
+        {
+            name: "Sausage Rolls",
+            price: 3000,
+            description: "2 Sausage Rolls"
+        },
+        {
+            name: "Portion Cake",
+            price: 6000,
+            description: "A Portion Cake"
+        },
+        {
+            name: "Doughnuts",
+            price: 3000,
+            description: "2 Doughnuts"
+        },
+        {
+            name: "Vegetable Rolls",
+            price: 3000,
+            description: "3 Vegetable Rolls"
+        },
+        {
+            name: "Pizza Ton",
+            price: "3000",
+            description: "2 Pizza Ton"
+        },
+        {
+            name: "Cheese Rolls",
+            price: 3000,
+            description: "2 Cheese Rolls"
+        },
+        {
+            name: "Pack of Chin-Chin",
+            price: 1500,
+            description: "1 Pack of Chin-Chin"
+        },
+        {
+            name: "Danish",
+            price: 3500,
+            description: "2 Danish"
+        },
+        {
+            name: "Pack of Pop Corn",
+            price: 2000,
+            description: "1 Pack of Pop Corn"
+        },
+        {
+            name: "Hash Brown",
+            price: 4800,
+            description: "Hash Brown"
+        }
+    ],
+    "Deep Fried": [
+        {
+            name: "150 GR. FISH FINGERS",
+            price: 6500,
+            description: ""
+        },
+        {
+            name: "ONION RINGS (5 Pcs)",
+            price: 3000,
+            description: ""
+        },
+        {
+            name: "FRENCH FRIES (200g)",
+            price: 4000,
+            description: ""
+        },
+        {
+            name: "FINGER YAM(200g)",
+            price: 6500,
+            description: ""
+        },
+        {
+            name: "SWEET POTATO CHIPS",
+            price: 4000,
+            description: ""
+        },
+        {
+            name: "FRIED PLANTAIN (8 Slices)",
+            price: 5000,
+            description: ""
+        }
+    ],
+    "Peppered": [
+        {
+            name: "Snails",
+            price: 18000,
+            description: ""
+        },
+        {
+            name: "Beef",
+            price: 7000,
+            description: ""
+        },
+        {
+            name: "Peppered Goat Meat",
+            price: 8500,
+            description: ""
+        },
+        {
+            name: "Gizzards",
+            price: 8000,
+            description: ""
+        },
+        {
+            name: "Chicken",
+            price: 8000,
+            description: ""
+        },
+        {
+            name: "Cow Leg",
+            price: 8000,
+            description: ""
+        },
+        {
+            name: "Croaker Fish PT",
+            price: 10000,
+            description: ""
+        },
+        {
+            name: "Prawns",
+            price: 24000,
+            description: ""
+        },
+        {
+            name: "Rock Fish PT",
+            price: 10000,
+            description: ""
+        },
+        {
+            name: "Turkey",
+            price: 12000,
+            description: ""
+        },
+        {
+            name: "Titus Fish PT",
+            price: 10000,
+            description: ""
         }
     ],
     "Pasta": [
@@ -77,18 +275,128 @@ const menuData = {
             name: "Pizza Margherita",
             price: 10000,
             description: "Tomato Sauce, Mozzarella Cheese, Oregano"
+        },
+        {
+            name: "Spaghetti Bolognaise",
+            price: 12000,
+            description: "The classical Pasta Dish With Tomatoes and Ground Beef, Gratinated with Parmesan Cheese And Served with Fresh Greens"
+        },
+        {
+            name: "Spaghetti and Jumbo Prawns",
+            price: 22000,
+            description: "To Cook this Local Dish of the Italian Tradition, We Pan Fry With Local Prawns in Olive Oil With Garlic, Dry Pepper and Spices Thickened with Cream"
+        },
+        {
+            name: "Mac and Cheese",
+            price: 18000,
+            description: "Macaroni, Cooking Cream, Butter with Cheddar Cheese"
+        },
+        {
+            name: "Pasta Alla Carbonara",
+            price: 12000,
+            description: "We Follow The Original Recipe and we Pan Fry BAcon in Olive Oil With Garlic Making Sauce with Scrambled Eggs and  Spices. Topped with Parmesan Cheese"
+        },
+        {
+            name: "Seafood Spaghetti",
+            price: 20000,
+            description: "A Dish of the Neapolitan Tradition, Cooked with the Local Seafood, Pan Fried in Olive Oil with Sliced Tomatoes, Garlic, And Spices Thickened With Blended Shrimp Sauce"
+        },
+        {
+            name: "Jollof Spaghetti",
+            price: 7000,
+            description: "A Dish Neapolitan Tradition cooked in Tomato Sauce"
+        },
+        {
+            name: "Garden Vegetable Spaghetti",
+            price: 12000,
+            description: "A Bounty From The Garden Fresh Seasonal Mix of Vegetables, Tomatoes, and Spices. Makes a Delicious Pasta Topping"
+        },
+        {
+            name: "Singapore Noodles",
+            price: 12000,
+            description: "From the Border of China, A Traditional Dish Made With Fried Curry Noodles Salted With Silced Fresh Vegetables, Baby Shrimps, And Shredded Beef accompanied with Chilli Sauce"
+        },
+        {
+            name: "Pizza Margherita",
+            price: 10000,
+            description: "With All the Flavours of the Springtime Hills With Tomato Sauce, Green Pepper, Onions, Mushrooms, Black Olives, Fresh Tomato, Sweet Corn, Mozzarella Cheese and Oreganao"
+        },
+        {
+            name: "Seafood Pizza",
+            price: 22000,
+            description: "With All thhhe Flavours from the Sea, We Serve you it with Tomato, Mixed Seafood, Mozzarella Cheese, and Basil Leaves"
+        },
+        {
+            name: "Panzerotti",
+            price: 11000,
+            description: "2 shaped South Italian Fried Pizza Stuffed With Tomato Sauce, Mozzarella Cheese, And Basil leaves"
+        },
+        {
+            name: "Seafood Platter",
+            price: 40000,
+            description: "A combination of Sea-food with choice of Fries, Yam/Sweet Potatoes"
+        },
+        {
+            name: "Spaghetti Meat Ball",
+            price: 12000,
+            description: "A Combo of Seafoods with the Option of Fries, Yam/Sweet Potatoes"
+        },
+        {
+            name: "Beef Bourguignon",
+            price: 9000,
+            description: "A Combo of Seafoods with the Option of Fries, Yam/Sweet Potatoes"
         }
     ],
     "Nigerian": [
         {
             name: "Jollof Rice",
             price: 5000,
-            description: "Traditional Recipe Cooked with Fresh Tomato Stew"
+            description: "Traditional Recipe Cooked with Fresh Tomato Stew, Fresh Pepper, And Garnished with Peas"
         },
         {
             name: "Coconut Rice",
             price: 5000,
             description: "Cooked with Coconut Milk and Powder"
+        },
+        {
+            name: "Basmati Fried Rice",
+            price: 7000,
+            description: "Basmati Rice is Salted in Spicy Vegetable Oil with Chopped Onions, Carrots and Green Beans"
+        },
+        {
+            name: "Special Fried Rice",
+            price: 12000,
+            description: "Basmati Rice is Salted in Spicy Vegetable Oil with Chopped Onions, Carrots, Green Beans, Diced chicken and Shrimp"
+        },
+        {
+            name: "Chinese Fried Rice",
+            price: 12000,
+            description: "Basmati Rice Salted in Spicy Vegetable Oil with Crumbled Eggs, Green Beans, Peas, Shrimp, Carrots and Chopped Onions"
+        },
+        {
+            name:"Jambalaya Rice",
+            price: 15000,
+            description: "Basmati Rice With Garlic, Chicken Breast, Sausage and Plantian In Tomato Sauce"
+        },
+        {
+            name: "Basmati Rice",
+            price: 5000,
+            description: "Basmati Rice"
+        },
+        {
+            name: "Ofada Rice With Sauce",
+            price: 12000,
+            description: "Ofada Rice, Ofada Sauce and Fried Plantain"
+        },
+        {
+            name: "White Rice",
+            price: 3000,
+            description: ""
+        },
+        {
+            name: "Portage Beans",
+            price: 8000,
+            desscription: ""
         }
     ],
     "Starters": [
@@ -101,6 +409,41 @@ const menuData = {
             name: "Shrimp Cocktail",
             price: 15000,
             description: "Local Shrimps on a Bed of Grated Carrots"
+        },
+        {
+            name: "Mixed Vegetable Salad",
+            price: 8000,
+            description: "A Healthy Choice of a Mix od =f seasonal vehetables served with italian dressing"
+        },
+        {
+            name: "STARTERS & DELIGHTS WARM SHREDDED STIR FRY CHICKEN SALAD",
+            price: 12000,
+            description: "An original recipe from the mountains of Thailand composed of fresh lettuce, carrot, and spring onion topped with shredded fried chicken with cashew nuts and served with a dip of soy sauce and green curry."
+        },
+        {
+            name: "COLESLAW",
+            price: 4000,
+            description: "A healthy choice for a mix of seasonal vegetables served with simple dressing."
+        },
+        {
+            name: "FRESH FRUIT SALAD",
+            price: 6000,
+            description: "A healthy choice for a mix of seasonal vegetables served with simple dressing."
+        },
+        {
+            name: "SWISS SALAD",
+            price: 9000,
+            description: "Created from the mind of our chef, an original recipe inspired by the land where our hotel was born. We mix for you fresh lettuce leaves with slices of green apple topped with small cubes of Gruyere cheese and served with French vinaigrette."
+        },
+        {
+            name: "GREEK SALAD",
+            price: 15000,
+            description: "In the nest of democracy, they created this mix of flavour that we need to give you. On a bed of fresh lettuce leaves topped with fresh slices of tomatoes, cucumber, and green pepper, we garnish with Kalamata black olives and the famous feta cheese, served with garlic dressing."
+        },
+        {
+            name: "CONCORD SALAD",
+            price: 8500,
+            description: "With a combination of green beans, peas, celery, green pepper, and onions on a bed of cabbage, we mix on top the chopped chicken with mayonnaise and garnish with boiled egg and croutons."
         }
     ],
     "Hot-starter": [
@@ -108,7 +451,112 @@ const menuData = {
             name: "Chicken",
             price: 10000,
             description: "Chicken pepper soup"
+        },
+        {
+            name: "Goat Meat",
+            price: 10000,
+            description: "Goat Meat pepper soup"
+        },
+        {
+            name: "Cow Leg",
+            price: 8000,
+            description: "Cow Leg pepper soup"
+        },
+        {
+            name: "Titus Fish",
+            price: 10000,
+            description: "Titus Fish  pepper soup"
+        },
+        {
+            name: "Portion Rock Fish",
+            price: 10000,
+            description: "Portion Rock pepper soup"
+        },
+        {
+            name: "Whole Rock Fish",
+            price: 20000,
+            description: "Whole Rock Fish pepper soup"
+        },
+        {
+            name: "Whole CatFish",
+            price: 20000,
+            description: "Whole CatFish pepper soup"
+        },
+        {
+            name: "Portion CatFish",
+            price: 10000,
+            description: "Portion CatFish pepper soup"
+        },
+        {
+            name: "French Onion Soup",
+            price: 13000,
+            description: "Topped With Gratinated Toast"
+        },
+        {
+            name: "Cream Of Mixed Vegetables",
+            price: 6000,
+            description: "A Mix Of Seasonal Fresh Vegetables, Blended and Thicked with Whipped Cream"
+        },
+        {
+            name: "Cream of Sweet Corn Soup",
+            price: 8500,
+            description: "We Blend the Sweet corn And Mix It With Stock And Spices Thickening it with Whipped Cream"
         }
+    ],
+    "Asian": [
+        {
+            name: "CHICKEN YAKITORI",
+            price: 12000,
+            description: "3 STICKS OF CHICKEN, ONIONS, AND GREEN PEPPER DIPPED IN A HOMEMADE SAUCE OF SOY SAUCE, GINGER AND GARLIC PASTE, SESAME OIL, SUGAR, AND SPRING ONION SERVED WITH FRIES"
+        },
+        {
+            name: "SWEET AND SOUR CHICKEN",
+            price: 15000,
+            description: "MARINATED DICED MEAT COOKED IN A MEDIUM CHILLI TOMATO SAUCE, VINEGAR, HONEY, AND LIME WITH SPRING ONION, GREEN PEPPER, AND PINEAPPLE SERVED WITH STEAM BASMATI RICE"
+        },
+        {
+            name: "CHICKEN CURRY",
+            price: 12000,
+            description: "ALL FLAVOURS OF THE INCREDIBLE INDIA FOR THIS BONELESS CHICKEN COOKED IN A MIX OF SPICES AND VEGETABLES THICKENED WITH CORNFLOWER AND SERVED WITH STEAMED BASMATI RICE OR HOMEMADE CHAPATI BREAD"
+        },
+        {
+            name: "FRIED RICE",
+            price: 5000,
+            description: "NIGERIAN RICE IS SALTED IN SPICY VEGETABLE OIL WITH CHOPPED ONIONS, CARROTS, AND GREEN BEANS."
+        },
+        {
+            name: "SPECIAL FRIED RICE",
+            price: 12000,
+            description: "BASMATI RICE IS SALTED IN SPICY VEGETABLE OIL WITH CHOPPED ONIONS, CARROTS, GREEN BEANS, DICED CHICKEN, SAUSAGE AND SHRIMP."
+        },
+        {
+            name: "CHINESE FRIED RICE",
+            price: 12000,
+            description: "BASMATI RICE SALTED IN SPICY VEGETABLE OIL WITH CRUMBLED EGGS, GREEN BEANS, PEAS, CARROTS, CHOPPED ONIONS, CHICKEN AND SHRIMP."
+        },
+        {
+            name: "Jabolaya Rice",
+            price: 15000,
+            description: "BASMATI RICE SALTED IN VEGETABLE OIL WITH GARLIC, MINCED MEAT, CRUMBLE EGGS, AND CABBAGE"
+        },
+        {
+            name: "BASMATI RICE",
+            price: 5000,
+            description: ""
+        },
+        {
+            name: "WHITE RICE",
+            price: 3000,
+            description: ""
+        },
+        {
+            name: "CHICKEN BREAST",
+            price: 13000,
+            description: ""
+        }
+    ],
+    "Special": [
+        
     ]
 };
 

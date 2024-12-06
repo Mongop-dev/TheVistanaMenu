e855eba14aa153bfcd646a94d6a71d0b71029cca
Igwe Ezekiel <igweezekiel2021@gmail.com>
2024-12-06 10:00:11 +0100

    push

--- scripts/c2-menu.js
+++ scripts/c2-menu.js
@@ -1,38 +1,694 @@
 // Menu data
 const menuData = {
-    "Swiss Cafe Menu": [
+    "Drinks": [
         {
-            name: "Swiss Cafe Early Bird",
+            name: "Medium Water(75cl)",
+            price: 1200,
+            description: ""
+        },
+        {
+            name: "Assorted Soft Drinks",
+            price: 1500,
+            description: ""
+        },
+        {
+            name: "Coke Zero",
+            price: 1500,
+            description: ""
+        },
+        {
+            name: "Pepsi Light",
+            price: 1500,
+            description: ""
+        },
+        {
+            name: "Soda Water",
+            price: 1500,
+            description: ""
+        },
+        {
+            name: "Bitter Lemon",
+            price: 1500,
+            description: ""
+        },
+        {
+            name: "Maltina",
+            price: 2500,
+            description: ""
+        },
+        {
+            name: "Amstel - Guinness",
+            price: 2500,
+            description: ""
+        },
+        {
+            name: "Power Horse",
+            price: 6000,
+            description: ""
+        },
+        {
+            name: "Red Bull",
+            price: 6000,
+            description: ""
+        },
+        {
+            name: "Bullet",
+            price: 6000,
+            description: ""
+        },
+        {
+            name: "Eviron",
+            price: 6000,
+            description: ""
+        },
+        {
+            name: "Monster",
             price: 7000,
-            description: "2 Fried Eggs, Tomato, Sausages, Baked Beans & Toasts with Coffee or Tea"
+            description: ""
         },
         {
-            name: "English Breakfast",
-            price: 10000,
-            description: "2 Scramble Eggs, Tomato, Sausages, Baked Beans & Toasts with Coffee or Tea"
-        },
-        {
-            name: "American Breakfast",
-            price: 18000,
-            description: "2 Croissants, Jam, Sausages, Baked Beans & with Coffee or Tea"
-        },
-        {
-            name: "Swiss Cafe Continental Breakfast",
-            price: 12000,
-            description: "FRESH JUICE, TOAST, OVEN FRESH BREADROLL, BUTTER & JAM, CEREALS, CHOICE OF COFFEE OR TEA"
-        },
-        {
-            name: "NIGERIAN BREAKFAST",
+            name: "Daravit Health Drink",
             price: 7000,
-            description: "TOAST, EGG SAUCE or KIDNEY SAUCE, PLANTAIN or YAM, COFFEE OR TEA"
+            description: ""
         },
         {
-            name: "Bircher Muesli",
-            price: 5500,
-            description: "The Classical swiss vitamin shot with oak flakes, yoghurt, and seasonal fruit"
-        }
-    
+            name: "Fayruz",
+            price: 1500,
+            description: ""
+        },
     ],
+    "Sandwiches": [
+        {
+            name: "Spicy Chicken Wings",
+            price: 10000,
+            description: "Glass/5cl = N10,000"
+        },
+        {
+            name: "All Peppered Chops",
+            price: 10000,
+            description: "Glass/5cl = N10,000"
+        },
+        {
+            name: "Chicken Quesadillas",
+            price: 10000,
+            description: "Served with caramelized onions, Guacamole, spicy salsa & chipotle"
+        },
+        {
+            name: "Cajun Shrimp Tacos",
+            price: 15000,
+            description: "Served with mango salsa, Jalapeno-lime, crème fraiche, Guacamole & cabbage"
+        },
+        {
+            name: "TED&CO Signature Pizza Slices ¼",
+            price: 2500,
+            description: "2500 / Slice"
+        },
+    ],
+    "Skillet": [
+        {
+            name: "Beef Stroganoff",
+            price: 12000,
+            description: "Tender beef strips in a creamy Mushroom Sauce served with Spaghetti or rice."
+        },
+        {
+            name: "Chicken Fajita",
+            price: 10000,
+            description: "Sizzling chicken bites served in corn tortillas."
+        },
+        {
+            name: "MAC & Cheese Skillet",
+            price: 10000,
+            description: "Served with sausages & bell pepper."
+        },
+        {
+            name: "Crispy Lasagna Skillet",
+            price: 11000,
+            description: ""
+        },
+        {
+            name: "Crispy Lasagna Skillet",
+            price: 12000,
+            description: ""
+        },
+        {
+            name: "Volcano Prawns",
+            price: 23000,
+            description: "An explosive treat served with Basmati rice."
+        },
+    ],
+    "Burgers": [
+        {
+            name: "TED & CO Signature Mini-Burger Trio",
+            price: 5000,
+            description: "Served with lettuce, tomato & Topped with Cheddar Cheese"
+        },
+        {
+            name: "Chicken Club Sandwich",
+            price: 8000,
+            description: "Layers full of flavour, served with French Fries"
+        },
+        {
+            name: "Rib Eye Steak Sandwich",
+            price: 25000,
+            description: "Grilled to your liking"
+        },
+        {
+            name: "Grilled Chicken Caesar Wrap",
+            price: 10000,
+            description: "Heavenly stuffed with lettuce & Tomato"
+        },
+    ],
+    "Cocktails": [
+        {
+            name: "TED & CO Mojito",
+            price: 10000,
+            description: "Rum, Mint, Lime, Sugar & Soda"
+        },
+        {
+            name: "Blue Margarita",
+            price: 8000,
+            description: "Blue Curacao, Triple Sec, Tequila & Lemon Juice"
+        },
+        {
+            name: "Cosmopolitan",
+            price: 8000,
+            description: "Vodka, Cointreau, Cranberry & Lime Juice"
+        },
+        {
+            name: "Long Island",
+            price: 12000,
+            description: "Rum, Vodka, Gin, Tequila, Cointreau, Lime & Coke"
+        },
+        {
+            name: "Manhattan",
+            price: 10000,
+            description: "Rum, Vodka, Gin, Tequila, Cointreau, Lime & Coke Ice, Jack Daniel, Vermouth (Martini dry) Angostura bitters and Cherry"
+        },
+        {
+            name: "Bloody Mary",
+            price: 10000,
+            description: ""
+        },
+        {
+            name: "Sex on the Beach",
+            price: 10000,
+            description: ""
+        },
+        {
+            name: "Tequilla Sunrise",
+            price: 8000,
+            description: ""
+        },
+        {
+            name: "Screwdriver",
+            price: 10000,
+            description: ""
+        }
+    ],
+    "Mocktails": [
+        {
+            name: "Virgin Mojito",
+            price: 5500,
+            description: ""
+        },
+        {
+            name: "Virgin Colada",
+            price: 5500,
+            description: ""
+        },
+        {
+            name: "Melon Berry",
+            price: 4000,
+            description: ""
+        },
+        {
+            name: "Chapman",
+            price: 5000,
+            description: ""
+        },
+        {
+            name: "Rock Shandy",
+            price: 5000,
+            description: ""
+        },
+        {
+            name: "Jamaica",
+            price: 4000,
+            description: ""
+        },
+        {
+            name: "Sun-Shine Delight",
+            price: 4000,
+            description: ""
+        },
+        {
+            name: "Milkshake",
+            price: 12000,
+            description: ""
+        },
+    ],
+    "Beer": [
+        {
+            name: "Heineken 600ml",
+            price: 4000,
+            description: ""
+        },
+        {
+            name: "Budweiser 600ml",
+            price: 3500,
+            description: ""
+        },
+        {
+            name: "Guiness Stout Big 600ml",
+            price: 4000,
+            description: ""
+        },
+        {
+            name: "Star Radler 600ml",
+            price: 2500,
+            description: ""
+        },
+        {
+            name: "Guilder 600ml",
+            price: 2500,
+            description: ""
+        },
+        {
+            name: "Goldberg 600ml",
+            price: 3500,
+            description: ""
+        },
+        {
+            name: "Desperados 600ml",
+            price: 3200,
+            description: ""
+        },
+        {
+            name: "Origin 600ml",
+            price: 3500,
+            description: ""
+        },
+        {
+            name: "Trophy 600ml",
+            price: 3200,
+            description: ""
+        },
+        {
+            name: "Trophy Stout 600ml",
+            price: 3000,
+            description: ""
+        },
+        {
+            name: "33 Export 600ml",
+            price: 3000,
+            description: ""
+        },
+        {
+            name: "Hero 600ml",
+            price: 3200,
+            description: ""
+        },
+        {
+            name: "Smirnoff Ice 250ml",
+            price: 3000,
+            description: ""
+        },
+        {
+            name: "Snap 250ml",
+            price: 2500,
+            description: ""
+        },
+        {
+            name: "Star 250ml",
+            price: 2500,
+            description: ""
+        },
+        {
+            name: "Small Stout 325ml",
+            price: 3000,
+            description: ""
+        },
+        {
+            name: "Medium Stout",
+            price: 3500,
+            description: ""
+        },
+        {
+            name: "Legend Beer 600ml",
+            price: 3500,
+            description: ""
+        },
+        {
+            name: "Smirnoff Ice Big 600ml",
+            price: 3500,
+            description: ""
+        },
+        {
+            name: "Hunter 325ml",
+            price: 3500,
+            description: ""
+        },
+        {
+            name: "Black Bullet 325ml",
+            price: 4500,
+            description: ""
+        },
+    ],
+    "Wine": [
+        {
+            name: "Nederburg Chardonnay",
+            price: 75000,
+            description: ""
+        },
+        {
+            name: "Patagonia",
+            price: 30000,
+            description: ""
+        },
+        {
+            name: "Massino",
+            price: 40000,
+            description: ""
+        },
+        {
+            name: "Carlo Rossi",
+            price: 50000,
+            description: ""
+        },
+        {
+            name: "Moutoun Cadet Bordeaux",
+            price: 50000,
+            description: ""
+        },
+        {
+            name: "Drostdy-Hof Claret Select",
+            price: 40000,
+            description: ""
+        },
+        {
+            name: "Escudo Rojo",
+            price: 50000,
+            description: ""
+        },
+        {
+            name: "Chateauneuf-de-Pape",
+            price: 60000,
+            description: ""
+        },
+        {
+            name: "Two Oceans",
+            price: 47000,
+            description: ""
+        },
+        {
+            name: "Mouton Cadet Sauvignon Blanc",
+            price: 50000,
+            description: ""
+        },
+        {
+            name: "Four Cousins Red",
+            price: 48000,
+            description: ""
+        },
+        {
+            name: "Four Cousins White",
+            price: 48000,
+            description: ""
+        },
+        {
+            name: "Andre Rose",
+            price: 55000,
+            description: ""
+        },
+        {
+            name: "Andre Brut",
+            price: 50000,
+            description: ""
+        },
+        {
+            name: "Frontera",
+            price: 48000,
+            description: ""
+        },
+        {
+            name: "Declan",
+            price: 48000,
+            description: ""
+        },
+        {
+            name: "Hardys Cabernet Sauvignon",
+            price: 30000,
+            description: ""
+        },
+        {
+            name: "Podere",
+            price: 25000,
+            description: ""
+        },
+        {
+            name: "4th Street Red",
+            price: 40000,
+            description: ""
+        },
+        {
+            name: "Castleflorit",
+            price: 30000,
+            description: ""
+        },
+        {
+            name: "Mini Drosdty Hof",
+            price: 5000,
+            description: ""
+        },
+        {
+            name: "Fox Brook Merlot",
+            price: 30000,
+            description: ""
+        },
+        {
+            name: "RiUnite Lamrusco",
+            price: 38000,
+            description: ""
+        },
+        {
+            name: "Fox Brook Carbonet Sarvinon",
+            price: 20000,
+            description: ""
+        },
+        {
+            name: "Moscato",
+            price: 20000,
+            description: ""
+        },
+        {
+            name: "Sweet Red",
+            price: 20000,
+            description: ""
+        },
+        {
+            name: "Nederburg Cabernet",
+            price: 65000,
+            description: ""
+        },
+        {
+            name: "Nederburg Sauvignon",
+            price: 70000,
+            description: ""
+        },
+        {
+            name: "Glass Of Four Cousins",
+            price: 10000,
+            description: ""
+        },
+        {
+            name: "Castillo Grande Red",
+            price: 15000,
+            description: ""
+        },
+        {
+            name: "Castillo Grande White",
+            price: 15000,
+            description: ""
+        },
+        {
+            name: "Castillo Grande Rose",
+            price: 15000,
+            description: ""
+        },
+        {
+            name: "Santa Alba Sweet",
+            price: 28000,
+            description: ""
+        },
+        {
+            name: "Cavalo Branco",
+            price: 30000,
+            description: ""
+        },
+        {
+            name: "Santa Sauvignon",
+            price: 30000,
+            description: ""
+        },
+        {
+            "name": "Santa-Alba",
+            "price": 28000,
+            "description": ""
+        },
+        {
+            "name": "4th Street White",
+            "price": 40000,
+            "description": ""
+        },
+        {
+            "name": "13 Secret",
+            "price": 48000,
+            "description": ""
+        },
+        {
+            "name": "Sandana",
+            "price": 25000,
+            "description": ""
+        },
+        {
+            "name": "Majesty",
+            "price": 30000,
+            "description": ""
+        },
+        {
+            "name": "Beacon Hill",
+            "price": 30000,
+            "description": ""
+        },
+        {
+            "name": "Feliza",
+            "price": 30000,
+            "description": ""
+        },
+        {
+            "name": "Stones",
+            "price": 30000,
+            "description": ""
+        },
+        {
+            "name": "Rubis",
+            "price": 70000,
+            "description": ""
+        },
+    ],
+    "Sweet": [
+        {
+            "name": "Agor Sweet Red Wine",
+            "price": 40000,
+            "description": ""
+        },
+        {
+            "name": "Rialto (Espana)",
+            "price": 8500,
+            "description": ""
+        },
+        {
+            "name": "Motivo",
+            "price": 10000,
+            "description": ""
+        },
+        {
+            "name": "Martini",
+            "price": 30000,
+            "description": ""
+        },
+        {
+            "name": "Andre Brut",
+            "price": 50000,
+            "description": ""
+        },
+        {
+            "name": "Moet & Chandon Imperial Brut",
+            "price": 200000,
+            "description": ""
+        },
+        {
+            "name": "Moet & Chandon Imperia Rose",
+            "price": 200000,
+            "description": ""
+        },
+        {
+            "name": "Romeo Brut",
+            "price": 200000,
+            "description": ""
+        },
+        {
+            "name": "Veuve Du Vernay (Ice)",
+            "price": 25000,
+            "description": ""
+        },
+        {
+            "name": "Veuve Du Vernay (Rose)",
+            "price": 40000,
+            "description": ""
+        },
+        {
+            "name": "Veuve Clicquot (Rose)",
+            "price": 250000,
+            "description": ""
+        },
+        {
+            "name": "Veuve Clicquot (Brut)",
+            "price": 220000,
+            "description": ""
+        },
+        {
+            "name": "Dom Perignon",
+            "price": 1000000,
+            "description": ""
+        },
+        {
+            "name": "Crystal",
+            "price": 400000,
+            "description": ""
+        },
+        {
+            "name": "Monkey shoulder",
+            "price": 90000,
+            "description": ""
+        },
+        {
+            "name": "Mateus Rose",
+            "price": 45000,
+            "description": ""
+        },
+        {
+            "name": "Mateus Brut",
+            "price": 45000,
+            "description": ""
+        },
+        {
+            "name": "B and G Cuvee Speciale",
+            "price": 35000,
+            "description": ""
+        },
+        {
+            "name": "Domino",
+            "price": 32000,
+            "description": ""
+        },
+        {
+            "name": "Feliza",
+            "price": 30000,
+            "description": ""
+        },
+        {
+            "name": "Castillo Grande",
+            "price": 30000,
+            "description": ""
+        },
+        {
+            "name": "Robertson Winery",
+            "price": 32000,
+            "description": ""
+        }
+    ],
+    
 };
 
 // DOM Elements
@@ -57,7 +713,7 @@ function renderMenuItems(category) {
 }
 
 // Initial render
-renderMenuItems("Swiss Cafe Menu");
+renderMenuItems("Drinks");
 
 // Category button click handler
 categoryButtons.addEventListener('click', (e) => {
